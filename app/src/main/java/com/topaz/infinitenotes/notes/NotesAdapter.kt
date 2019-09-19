7ebc89837f0d1277349c18e6c6e4372b21eca0f5
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-09-19 22:35:18 +0300

    added listener for clicks on items

--- app/src/main/java/com/topaz/infinitenotes/notes/NotesAdapter.kt
+++ app/src/main/java/com/topaz/infinitenotes/notes/NotesAdapter.kt
@@ -1,6 +1,7 @@
 package com.topaz.infinitenotes.notes
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -8,27 +9,36 @@ import androidx.recyclerview.widget.RecyclerView
 import com.topaz.infinitenotes.database.Note
 import com.topaz.infinitenotes.databinding.ListItemNoteBinding
 
-class NotesAdapter : ListAdapter<Note, NotesAdapter.ViewHolder>(NoteDiffCallback()) {
+interface OnItemClickListener {
+    fun onClick(vh: View, item: Note)
+}
+
+class NotesAdapter(private val listener: OnItemClickListener) :
+    ListAdapter<Note, NotesAdapter.ViewHolder>(NoteDiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.from(parent)
+        return ViewHolder.from(parent, listener)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
-    class ViewHolder private constructor(private val binding: ListItemNoteBinding) :
+    class ViewHolder private constructor(
+        private val binding: ListItemNoteBinding,
+        private val listener: OnItemClickListener
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Note) {
             binding.note = item
+            binding.root.setOnClickListener { listener.onClick(binding.root, item) }
             binding.executePendingBindings()
         }
 
         companion object {
-            fun from(parent: ViewGroup): ViewHolder {
+            fun from(parent: ViewGroup, listener: OnItemClickListener): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ListItemNoteBinding.inflate(layoutInflater, parent, false)
-                return ViewHolder(binding)
+                return ViewHolder(binding, listener)
             }
         }
     }
--- app/src/main/java/com/topaz/infinitenotes/notes/NotesFragment.kt
+++ app/src/main/java/com/topaz/infinitenotes/notes/NotesFragment.kt
@@ -2,18 +2,18 @@ package com.topaz.infinitenotes.notes
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
 
 import com.topaz.infinitenotes.R
 import com.topaz.infinitenotes.database.Note
 import com.topaz.infinitenotes.database.NotesDatabase
 import com.topaz.infinitenotes.databinding.FragmentNotesBinding
+import com.topaz.infinitenotes.newnotes.NewnoteFragment
 
 /**
  * A simple [Fragment] subclass.
@@ -27,6 +27,8 @@ class NotesFragment : Fragment() {
         val binding: FragmentNotesBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_notes, container, false)
 
+        setHasOptionsMenu(true)
+
         val dataSource =
             NotesDatabase.getInstance(requireNotNull(this.activity).application).noteDatabaseDao
         val viewModelFactory = NotesViewModelFactory(dataSource)
@@ -36,12 +38,32 @@ class NotesFragment : Fragment() {
 
         binding.notesViewModel = notesViewModel
 
-        val adapter = NotesAdapter()
+        val adapter = NotesAdapter(object : OnItemClickListener{
+            override fun onClick(vh: View, item: Note) {
+                val fragment = NewnoteFragment()
+                val bundle = Bundle()
+                bundle.putParcelable("NOTE_KEY", item)
+                fragment.arguments = bundle
+                val transaction = fragmentManager?.beginTransaction() ?: return
+                transaction.replace(R.id.nav_host_fragment, fragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+            }
+
+        })
+        binding.notesList.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                DividerItemDecoration.VERTICAL
+            )
+        )
         binding.notesList.adapter = adapter
+
+
 
         notesViewModel.notes.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.submitList(it)
+                adapter.submitList(it.sortedBy { it.timeEdit })
             }
         })
         binding.lifecycleOwner = this
@@ -50,4 +72,21 @@ class NotesFragment : Fragment() {
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_notes, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_new -> {
+                val fragment = NewnoteFragment()
+                val transaction = fragmentManager?.beginTransaction() ?: return false
+                transaction.replace(R.id.nav_host_fragment, fragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
