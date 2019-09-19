b6d4b2b6407e3ee0641a86c1bcd62bea2d420bca
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-09-19 22:34:58 +0300

    Fragment to create notes

---
+++ app/src/main/java/com/topaz/infinitenotes/newnotes/NewnoteFragment.kt
@@ -0,0 +1,45 @@
+package com.topaz.infinitenotes.newnotes
+
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+
+import com.topaz.infinitenotes.R
+import com.topaz.infinitenotes.database.Note
+import com.topaz.infinitenotes.databinding.FragmentNewnoteBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class NewnoteFragment : Fragment() {
+
+    private var note: Note? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentNewnoteBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_newnote, container, false)
+        setHasOptionsMenu(true)
+        note = arguments?.getParcelable("NOTE_KEY")
+        binding.note = note
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_new_note, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.done -> {
+                activity?.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
