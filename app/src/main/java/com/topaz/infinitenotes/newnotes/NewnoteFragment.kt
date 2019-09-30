827d31debf64e33845f9b8b6b93274a627c96895
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-09-30 22:00:56 +0300

    Added viewmodel

--- app/src/main/java/com/topaz/infinitenotes/newnotes/NewnoteFragment.kt
+++ app/src/main/java/com/topaz/infinitenotes/newnotes/NewnoteFragment.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 
 import com.topaz.infinitenotes.R
 import com.topaz.infinitenotes.database.Note
+import com.topaz.infinitenotes.database.NotesDatabase
 import com.topaz.infinitenotes.databinding.FragmentNewnoteBinding
 
 /**
@@ -25,6 +27,12 @@ class NewnoteFragment : Fragment() {
         setHasOptionsMenu(true)
         note = arguments?.getParcelable("NOTE_KEY")
         binding.note = note
+
+        val dataSource =
+            NotesDatabase.getInstance(requireNotNull(this.activity).application).noteDatabaseDao
+        val viewModelFactory = NewnoteViewModelFactory(dataSource)
+        val viewModel = ViewModelProviders.of(this).get(NewnoteViewModel::class.java)
+
         return binding.root
     }
 
