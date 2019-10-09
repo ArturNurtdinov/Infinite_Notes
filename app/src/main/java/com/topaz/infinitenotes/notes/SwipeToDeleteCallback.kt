9e6417b35e5acfe0e148afd2513c389b45b7a358
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-10-09 20:53:52 +0300

    Drawing delete icon when item is being dragged

--- app/src/main/java/com/topaz/infinitenotes/notes/SwipeToDeleteCallback.kt
+++ app/src/main/java/com/topaz/infinitenotes/notes/SwipeToDeleteCallback.kt
@@ -1,15 +1,78 @@
 package com.topaz.infinitenotes.notes
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.topaz.infinitenotes.R
 
-class SwipeToDeleteCallback(val delete: (Int) -> Unit, dragDirs: Int, swipeDirs: Int): ItemTouchHelper.SimpleCallback(dragDirs, swipeDirs) {
+
+class SwipeToDeleteCallback(
+    context: Context,
+    val delete: (Int) -> Unit,
+    dragDirs: Int,
+    swipeDirs: Int
+) :
+    ItemTouchHelper.SimpleCallback(dragDirs, swipeDirs) {
+    private val icon = ContextCompat.getDrawable(context, R.drawable.ic_delete_white_36dp)
+    private val background = ColorDrawable(Color.RED)
+
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+        val itemView = viewHolder.itemView
+        val offset = 20
+        if (icon == null) return
+        val iconMargin = (itemView.height - icon.intrinsicHeight) / 2
+        val iconTop = itemView.top + (itemView.height - icon.intrinsicHeight) / 2
+        val iconBottom = itemView.top + icon.intrinsicHeight
+        when {
+            dX > 0 -> {
+                val iconLeft = itemView.left + iconMargin + icon.intrinsicWidth
+                val iconRight = itemView.left + iconMargin
+                icon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
+                background.setBounds(
+                    itemView.left,
+                    itemView.top,
+                    itemView.left + dX.toInt() + offset,
+                    itemView.bottom
+                )
+            }
+            dX < 0 -> {
+                val iconLeft = itemView.right - iconMargin - icon.intrinsicWidth
+                val iconRight = itemView.right - iconMargin
+                icon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
+                background.setBounds(
+                    itemView.right + dX.toInt() - offset,
+                    itemView.top,
+                    itemView.right,
+                    itemView.bottom
+                )
+            }
+            else -> {
+                background.setBounds(0, 0, 0, 0)
+            }
+        }
+        background.draw(c)
+        icon.draw(c)
+    }
+
     override fun onMove(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder,
         target: RecyclerView.ViewHolder
     ): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return true
     }
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
