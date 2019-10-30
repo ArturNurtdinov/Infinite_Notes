9011150590f719dff7897b0c013c13d45894839c
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-10-30 23:48:56 +0300

    Mappers

---
+++ app/src/main/java/com/infinitevoid/infinitenotes/mappers/NoteMapper.kt
@@ -0,0 +1,20 @@
+package com.infinitevoid.infinitenotes.mappers
+
+import com.infinitevoid.infinitenotes.database.NoteRest
+import com.infinitevoid.infinitenotes.domain.Note
+
+fun mapNoteRestToNote(noteRest: NoteRest): Note {
+    with(noteRest) {
+        return Note(noteID, timeEdit, title, content)
+    }
+}
+
+fun mapNoteToNoteRest(note: Note): NoteRest {
+    with(note) {
+        return NoteRest(noteID, timeEdit, title, content)
+    }
+}
+
+fun mapNoteRestListToNoteList(noteRestList: List<NoteRest>): List<Note> {
+    return noteRestList.map { mapNoteRestToNote(it) }
+}
