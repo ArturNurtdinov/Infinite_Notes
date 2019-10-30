3ec0716ca20415f938422f199f96d3247253d95a
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-10-30 23:48:43 +0300

    Domain layer

---
+++ app/src/main/java/com/infinitevoid/infinitenotes/domain/Note.kt
@@ -0,0 +1,15 @@
+package com.infinitevoid.infinitenotes.domain
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Note(
+    var noteID: Long = 0L,
+
+    var timeEdit: Long = System.currentTimeMillis(),
+
+    var title: String = "Title",
+
+    var content: String = "Content"
+) : Parcelable
