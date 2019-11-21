76a5000b5db6219ad2ba0a15c4924363cbefd85f
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-11-21 15:12:55 +0300

    Added test for mapping list

--- app/src/test/java/com/infinitevoid/infinitenotes/ExampleUnitTest.kt
+++ app/src/test/java/com/infinitevoid/infinitenotes/ExampleUnitTest.kt
@@ -1,6 +1,8 @@
 package com.infinitevoid.infinitenotes
 
+import com.infinitevoid.infinitenotes.database.NoteRest
 import com.infinitevoid.infinitenotes.domain.Note
+import com.infinitevoid.infinitenotes.mappers.mapNoteRestListToNoteList
 import com.infinitevoid.infinitenotes.mappers.mapNoteToNoteRest
 import org.junit.Test
 
@@ -16,6 +18,26 @@ class ExampleUnitTest {
     fun mappingNote() {
         val note = Note(4, 18874032, "Note Title", "Note Content")
 
-        assertEquals(note, mapNoteToNoteRest(note))
+        val expected = NoteRest(4, 18874032, "Note Title", "Note Content")
+
+        assertEquals(expected, mapNoteToNoteRest(note))
+    }
+
+    @Test
+    fun mappingListOfNotes() {
+        val note1 = Note(1, 188740321, "Note Title 1", "Note Content 1")
+        val note2 = Note(2, 188740322, "Note Title 2", "Note Content 2")
+        val note3 = Note(3, 188740323, "Note Title 3", "Note Content 3")
+
+        val expected = listOf(note1, note2, note3)
+
+        val listToMap = listOf(
+            mapNoteToNoteRest(note1),
+            mapNoteToNoteRest(note2),
+            mapNoteToNoteRest(note3)
+        )
+
+        assertEquals(expected, mapNoteRestListToNoteList(listToMap))
+
     }
 }
