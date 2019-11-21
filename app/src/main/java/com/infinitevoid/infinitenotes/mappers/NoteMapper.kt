0d07c1432fc7545b661d56c42615481398aa5467
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-11-21 23:26:04 +0300

    fix: Change constructor of data classes

--- app/src/main/java/com/infinitevoid/infinitenotes/mappers/NoteMapper.kt
+++ app/src/main/java/com/infinitevoid/infinitenotes/mappers/NoteMapper.kt
@@ -4,11 +4,11 @@ import com.infinitevoid.infinitenotes.database.NoteRest
 import com.infinitevoid.infinitenotes.domain.Note
 
 fun mapNoteRestToNote(noteRest: NoteRest): Note {
-    with(noteRest) { return Note(noteID, timeEdit, title, content) }
+    with(noteRest) { return Note(noteID, timeEdit, title, content, imageURI) }
 }
 
 fun mapNoteToNoteRest(note: Note): NoteRest {
-    with(note) { return NoteRest(noteID, timeEdit, title, content) }
+    with(note) { return NoteRest(noteID, timeEdit, title, content, imageURI) }
 }
 
 fun mapNoteRestListToNoteList(noteRestList: List<NoteRest>): List<Note> {
